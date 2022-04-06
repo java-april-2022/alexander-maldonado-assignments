047f28e5ab2f324b4ad75e1af2ec99e1e55898cf
Alexander-Maldonado <alex_ahc@yahoo.com>
2022-04-05 23:18:19 -0700

    Commiting practice assignments

---
+++ Week1/Extra Practice/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class FirstJavaProgram {
+    public static void main(String[] arg){
+        //"\r\n\" this is used in Windows between strings so the next string knows to continue on the following line
+        System.out.println("My name is Alexander Maldonado" + "\r\n" + "I am 28 years old" + "\r\n" + "My hometown is Santa Maria, CA");
+    }
+}
---
+++ Week1/Greetings/Test.java
@@ -0,0 +1,9 @@
+//File name must be the same as the class name
+//Every file is a class
+public class Test {
+    // ever Java program will have one and only one class file that has a main method
+    public static void main(String[] args) {
+        //any code you write initially will be in between these two inner curly brackets(inside the method)
+        System.out.println("Hello world!");
+    }
+}
